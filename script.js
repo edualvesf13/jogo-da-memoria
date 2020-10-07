b20e345a7c0978c36dc4f345f0760d04ce8acbe6
Eduardo ALves Ferreira <edualvesf13@gmail.com>
2020-10-07 19:35:45 -0300

    Add function resetBoard.

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const cards = document.querySelectorAll('.card')
 let hasFlippedCard = false
 let firstCard, secondCard
+let lockBoard = false
 
 
 function unflipCards() {
+    lockBoard = true
+
     setTimeout( () => {
         firstCard.classList.remove('flip')
         secondCard.classList.remove('flip')
+
+        resetBoard()
     }, 1500 )
 }
 
 function disableCards() {
     firstCard.removeEventListener('click', flipCard)
     secondCard.removeEventListener('click', flipCard)
+
+    resetBoard()
 }
 
 function checkForMatch() {
@@ -25,6 +32,10 @@ function checkForMatch() {
 }
 
 function flipCard() {
+    if(lockBoard) return
+
+    if(this === firstCard) return
+
     this.classList.add('flip')
 
     if (!hasFlippedCard) {
@@ -37,6 +48,11 @@ function flipCard() {
     hasFlippedCard = false
 
     checkForMatch()
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false]
+    [firstCard, secondCard] = [null, null]
 }
 
 cards.forEach( card => card.addEventListener('click', flipCard) )
