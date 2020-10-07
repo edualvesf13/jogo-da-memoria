01d45a7093a5942c4dca185e47f6c7c1b70c2af0
Eduardo ALves Ferreira <edualvesf13@gmail.com>
2020-10-07 19:24:50 -0300

    Add function flipCard and CheckForMatch in Js.

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 const cards = document.querySelectorAll('.card')
+let hasFlippedCard = false
+let firstCard, secondCard
+
+
+function unflipCards() {
+    setTimeout( () => {
+        firstCard.classList.remove('flip')
+        secondCard.classList.remove('flip')
+    }, 1500 )
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard)
+    secondCard.removeEventListener('click', flipCard)
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards()
+        return
+    }
+
+    unflipCards()
+}
 
 function flipCard() {
     this.classList.add('flip')
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true
+        firstCard = this
+        return
+    }
+
+    secondCard = this
+    hasFlippedCard = false
+
+    checkForMatch()
 }
 
 cards.forEach( card => card.addEventListener('click', flipCard) )
